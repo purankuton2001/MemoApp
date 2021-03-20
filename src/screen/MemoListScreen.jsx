4d573a9a702d065e20accdab0cb3d9b1524b04aa
purankuton2001 <purankuton2001@icloud.com>
2021-03-20 11:58:35 +0900

    create Loading component

--- src/screen/MemoListScreen.jsx
+++ src/screen/MemoListScreen.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import {
+  View, StyleSheet, Alert, Text,
+} from 'react-native';
 
 import firebase from 'firebase';
 import { useState } from 'react/cjs/react.development';
 import MemoList from '../components/MemoList';
 import CircleButton from '../components/CircleButton';
 import LogoutButton from '../components/LogoutButton';
+import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function MemoListScreen(props) {
   const { navigation } = props;
   const [memos, setMemos] = useState([]);
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => <LogoutButton />,
@@ -21,6 +26,7 @@ export default function MemoListScreen(props) {
     const { currentUser } = firebase.auth();
     let unsubscribe = () => {};
     if (currentUser) {
+      setLoading(true);
       const ref = db.collection(`users/${currentUser.uid}/memos`).orderBy('updatedAt', 'desc');
       unsubscribe = ref.onSnapshot((snapshot) => {
         const userMemos = [];
@@ -33,15 +39,27 @@ export default function MemoListScreen(props) {
           });
         });
         setMemos(userMemos);
+        setLoading(false);
       }, (error) => {
         console.log(error.code);
+        setLoading(false);
         Alert.alert('メモの読み込みに失敗しました。');
       });
     }
     return unsubscribe;
   },
   []);
-
+  if (memos.length === 0) {
+    return (
+      <View style={emptyStyles.container}>
+        <Loading isLoading={isLoading} />
+        <View style={emptyStyles.inner}>
+          <Text style={emptyStyles.text}>新しいメモを作成しよう！</Text>
+          <Button label="メモを作成" style={emptyStyles.button} onPress={() => { navigation.navigate('MemoCreate'); }} />
+        </View>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <MemoList memos={memos} />
@@ -54,5 +72,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFFFFF',
+  },
+});
+
+const emptyStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inner: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  button: {
+    alignSelf: 'center',
   },
 });
